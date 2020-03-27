818bee36053d0dfdf865b18fed9fed3872adf0e4
johnson-chou8 <johnson.chou6888@gmail.com>
2020-03-27 12:48:19 -0700

    Updated compile issues.

--- frontend/src/Redux/store.js
+++ frontend/src/Redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger';
-import rootReducer from './reducers/index';
+import rootReducer from './Reducers/index';
 
 const logger = createLogger()
 const store = createStore(rootReducer, applyMiddleware(thunk, logger));
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import Store from './redux/store';
+import Store from './Redux/store';
 
 ReactDOM.render(
   <Provider store={Store}>
