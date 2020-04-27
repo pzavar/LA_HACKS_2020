1fe3883a95054b5200bdc43ee6cabab8746c9e77
Ashkan Faghihi <ashkan117@yahoo.com>
2020-04-27 14:01:09 -0700

    Changed logic to store a weeks into our database

---
+++ backend/models/weeks.js
@@ -0,0 +1,16 @@
+// Week Schema
+
+var mongoose = require('mongoose');
+//Define a schema
+var Schema = mongoose.Schema;
+
+var WeekSchema = new mongoose.Schema({
+    userId:           String,
+	mealIds: [String],
+    week: 	[Schema.Types.Mixed]
+    // goal:               { type: String },
+    // username:           { type: String },
+    // password:           { type: String }
+});
+
+module.exports = Week = mongoose.model("Week", WeekSchema);
