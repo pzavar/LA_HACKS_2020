9bcdbc07dd9aac7df01e9aabf5506788da3be283
Ashkan Faghihi <ashkan117@yahoo.com>
2020-05-08 10:57:56 -0700

    merged with master,basic sort by cost endpoint meals/budget

--- backend/routes/meals.js
+++ backend/routes/meals.js
@@ -9,51 +9,32 @@ var mongoose = require('mongoose')
 
 var Week = require('../models/weeks')
 var Meal = require('../models/meals')
-// Get recipes by meal
-// All breakfast
-// All lunch
-// All dinner
 
+// Ingredient = comma seperated list
+router.get('/budget',async function(req,res,next){
+	var search = await fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${config.spoonacularApiKey}&ingredients=apples,+flour&number=2`)
+	var json = await search.json()
+  
+  ids = []
+  json.forEach(recipe => {
+    //Extract the id of each recipe
+    ids.push(recipe["id"])
+  })
 
-///* GET home page. */
-//router.get('/week', async function(req, res, next) {
-//	console.log(req)
-//	var searchList = ["chicken", "steak", "salad","turkey","spinach","sandwich"]
-//	var searchOne = await fetch(`https://api.edamam.com/search?q=${searchList[0]}&app_id=${config.recipeId}&app_key=${config.recipeApiKey}&from=0&to=21`)
-//	var json = await searchOne.json()
-//	var hits = json["hits"]
-//	console.log("Search hits")
-//	console.log(hits)
+	var testIds = ["715538,716429"]
+	var ingredientsBulk = await fetch(`https://api.spoonacular.com/recipes/informationBulk?apiKey=${config.spoonacularApiKey}&ids=${testIds}&includeNutrition=true`)
+	var bulkJson = await ingredientsBulk.json()
+	console.log("Bulk")
+	//Returns list of maps
+	console.log(bulkJson)
+  budgetSortedRecipe = []
+  bulkJson.forEach(recipe => {
+    budgetSortedRecipe.push(recipe["pricePerServing"])
+  })
+  budgetSortedRecipe.sort((one,two) => one < two)
 
-//	var week = []
-//	for(var i = 0; i < 7; i++){
-//		for(var j = 0; j < 3; j++){
-//			var mealType = "breakfast"
-//			if(j == 1) mealType = "lunch"
-//			else if(j == 2) mealType = "dinner"
-//			//A little bit of a magic number but this us numbers 0 -20 inclusive
-//			var meal = hits[j+(3 *i)]["recipe"]
-//			console.log(meal)
-//			var day = {}
-//			day["label"] = meal["label"]
-//			console.log(meal["label"])
-//			day["image"] = meal["image"]
-//			day["source"] = meal["source"]
-//			day["url"] = meal["url"]
-//			day["calories"] = meal["calories"]
-//			console.log(meal["calories"])
-//			day["yield"] = meal["yield"]
-//			console.log(meal["yield"])
-//			day["healthLabels"] = meal["healthLabels"]
-//			console.log(meal["healthLabels"])
-//			day["mealType"] = mealType
-//			console.log(mealType)
-//			day["recipe"] = meal["ingredientLines"]
-//			week.push(day)
-//		}
-//	}
-//	res.send(week)
-//});
+	res.send(budgetSortedRecipe)
+});
 
 //id: 17281,
 // title: "Spicy Tuna Tartare",
@@ -66,7 +47,7 @@ var Meal = require('../models/meals')
 //https://api.spoonacular.com/recipes/716429/information?apiKey=YOUR-API-KEY&includeNutrition=true.
 // json["week"]["monday"]["meals"][0,1,2]
 // json["week"]["monday"]["nutrients"]
-router.get('/week', verifyToken, async function(req, res, next) {
+router.get('/week', async function(req, res, next) {
 	console.log(config.spoonacularApiKey)
 	var search = await fetch(`https://api.spoonacular.com/mealplanner/generate?apiKey=${config.spoonacularApiKey}&timeFrame=week`)
 	console.log("Test")
