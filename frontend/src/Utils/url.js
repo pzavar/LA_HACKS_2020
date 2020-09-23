40a98d8c799bb443b47845ed1f39facf1de6d5db
Ashkan Faghihi <ashkan117@yahoo.com>
2020-09-22 20:18:48 -0700

    updated url

--- frontend/src/Utils/url.js
+++ frontend/src/Utils/url.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-export const url = 'http://localhost:4000'
-
 export const api = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: 'forkandspatula.com',
 })
 
